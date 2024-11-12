27cedf7d520104a1a9ebf367e9bfbeb0bb02e390
brx-nbif <blondeau.nbif@mail.ru>
2024-11-12 15:31:08 +0100

    feat: add jwt and start authentification

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
 import { router } from "./router";
 
 const app = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (_, res) {
   res.status(200).json({ message: "API is running" });
 });
 
-app.use(express.json());
 app.use("/api", router);
 
 export default app;
